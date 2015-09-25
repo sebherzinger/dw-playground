43a002ef2822e0f7fb1208754ab167cac32e64db
Sebastian Lukas Herzinger <sebastian.herzinger@gmail.com>
2015-09-25 06:57:31 +0200

    - added modules - added animations onLoad

--- app/js/main.js
+++ app/js/main.js
@@ -1,42 +1,38 @@
-$(document).ready( function() {
-	
-	$(window).scroll( function() {	
-		
-		if ( $(this).scrollTop() > 1 ) {  
+$(function() {
+
+	window.sr = new scrollReveal();
+
+	$(window).scroll( function() {
+
+		if ( $(this).scrollTop() > 1 ) {
 			$('#header').addClass("sticky");
 			//$('.main-nav-item:first-child').addClass('active');
 		} else{
 			$('#header').removeClass("sticky");
-			//$('.main-nav-item:first-child').removeClass('active');
 		}
-		
+
+
 	});
-	
-	$('section').viewportChecker({
-		classToAdd: 'visible',
-		offset: 0,
-		callbackFunction: function(e, action) {
-			//$('.main-nav-item').removeClass('active');
-			//$('*[data-name="'+ e.attr('id') +'"]').parent().addClass('active');
-		}
+
+	$('section').each(function() {
+
+
 	});
-	
-	 window.sr = new scrollReveal();
-	
+
 	$('#js-toTop').on('click', function(e) {
-		$("html, body").animate({ 
+		$("html, body").animate({
 			scrollTop: 0 }, "slow"
 		);
-		
+
 		e.preventDefault();
 	});
-	
+
 	$('.js-scrollTo').on('click', function(e) {
 		$('html, body').animate({
 			scrollTop: $('#' + $(this).data('name')).offset().top - 89
 		}, "slow");
-		
+
 		e.preventDefault();
 	});
-	
+
 });
